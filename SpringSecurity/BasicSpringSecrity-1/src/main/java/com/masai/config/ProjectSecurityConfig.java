f9566b1b59b21fcedc4b0b1b6cafb84a4c368175
GandhamRaghavendra <graghavendramutry13@gmail.com>
2023-04-07 16:13:25 +0530

    springSecurityInMemoryType

---
+++ SpringSecurity/BasicSpringSecrity-1/src/main/java/com/masai/config/ProjectSecurityConfig.java
@@ -0,0 +1,44 @@
+package com.masai.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class ProjectSecurityConfig {
+
+	@Bean
+	public SecurityFilterChain masaiSecurityConfig(HttpSecurity http) throws Exception {
+
+		http.authorizeHttpRequests((auth) -> auth
+				.requestMatchers("/masai/welcomeP").authenticated()
+				.requestMatchers("/masai/welcome").permitAll()
+				// this will only allow admin to access this url..!
+				.requestMatchers("/masai/admin").hasAuthority("admin"))
+		        .httpBasic();
+
+		return http.build();
+	}
+	
+	@Bean
+	public InMemoryUserDetailsManager userDetails() {
+	
+		InMemoryUserDetailsManager userDetailsService = new InMemoryUserDetailsManager();
+	  UserDetails admin = User.withUsername("admin").password("12345").authorities("admin").build();
+	    UserDetails user = User.withUsername("user").password("12345").authorities("read").build();
+	    userDetailsService.createUser(admin);
+	    userDetailsService.createUser(user);
+	    return userDetailsService;
+	}
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return NoOpPasswordEncoder.getInstance(); // Not recommended
+	}
+}
