4de0ed1a99f8e1d6d40964f761957a018a538293
GandhamRaghavendra <graghavendramutry13@gmail.com>
2023-04-08 12:27:29 +0530

    SpringSecurityWithJwtRole

---
+++ SpringSecurity/SpringSecurityWithJwt/src/main/java/com/masai/model/MyUserDetails.java
@@ -0,0 +1,67 @@
+package com.masai.model;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public class MyUserDetails implements UserDetails{
+
+	@Autowired
+	private Employee employee;
+	
+	public MyUserDetails(Employee employee) {
+		this.employee=employee;
+	}
+	
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		
+		Collection<GrantedAuthority> authorities = new ArrayList<>();
+		
+		SimpleGrantedAuthority role =new SimpleGrantedAuthority(employee.getRole());
+		
+		authorities.add(role);
+		
+		return authorities;
+	}
+
+	@Override
+	public String getPassword() {
+		return employee.getPassword();
+	}
+
+	@Override
+	public String getUsername() {
+		return employee.getUserName();
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	
+}
